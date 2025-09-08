f7e2a61ea4e50a4dcfdd87b4723b7b0528067a80
Víctor Rico <victor@gmail.com>
2025-09-08 20:19:33 +0200

    feat: Add funds fetch helper

---
+++ packages/client/src/lib/api/funds.ts
@@ -0,0 +1,35 @@
+import { type Fund } from "../../../../server/server/data/funds";
+const API_URL = "http://localhost:3000";
+
+export type GetFundsParams = {
+  page?: number;
+  limit?: number;
+};
+
+export type GetFundsResponse = {
+  pagaination: {
+    page: number;
+    limit: number;
+    totalFunds: number;
+    totalPages: number;
+  };
+  data: Fund[];
+};
+
+export async function getFunds({
+  page = 1,
+  limit = 20,
+}: GetFundsParams = {}): Promise<GetFundsResponse> {
+  const searchParams = new URLSearchParams({
+    page: String(page),
+    limit: String(limit),
+  });
+
+  const data = await fetch(`${API_URL}/funds?${searchParams.toString()}`);
+
+  if (!data.ok) {
+    throw new Error(`Failes to fetch funds: ${data.status} ${data.statusText}`);
+  }
+
+  return await data.json();
+}
