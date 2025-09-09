90e906cb4f2d26263cbe8a75c43d2a945ade91df
Víctor Rico <victor@gmail.com>
2025-09-09 11:51:50 +0200

    feat: Prepare pagination routes

--- packages/client/next.config.ts
+++ packages/client/next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
-
-export default nextConfig;
+export default {
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/funds",
+        permanent: true,
+      },
+    ];
+  },
+} satisfies NextConfig;
---
+++ packages/client/src/app/funds/page.tsx
@@ -0,0 +1,24 @@
+import Container from "@/components/Container";
+import FundsTable from "@/components/FundsTable";
+import { Pagination } from "@/components/Pagination";
+import { getFunds } from "@/lib/api/funds";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "MyInvestor - funds",
+};
+
+type HomeParams = {
+  searchParams: Promise<Record<string, string | string[] | undefined>>;
+};
+
+export default async function Home({ searchParams }: HomeParams) {
+  const filters = await searchParams;
+  const funds = await getFunds(filters);
+
+  return (
+    <Container>
+      <FundsTable data={funds.data} />
+    </Container>
+  );
+}
